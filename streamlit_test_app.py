669c08cb69f11568fe04d3c0cc8da16ce070c4c5
fernandop1987 <fernando.pedroarena@gmail.com>
2024-08-21 19:37:08 -0300

    Update streamlit_test_app.py

--- streamlit_test_app.py
+++ streamlit_test_app.py
@@ -64,12 +64,11 @@ df_mapa['DIA_SEMANA'] = pd.Categorical(df_mapa['DIA_SEMANA'], categories=orden_d
 orden_horas = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
 df_mapa['HORA'] = pd.Categorical(df_mapa['HORA'], categories=orden_horas, ordered=True)
 
+df_mapa['total_delitos'] = pd.to_numeric(df_mapa['total_delitos'], errors='coerce')
 
 # Crear una tabla pivotada para contar la cantidad de delitos por día de la semana y hora
 tabla_calor = df_mapa.pivot_table(index='DIA_SEMANA', columns='HORA', values='total_delitos', aggfunc='sum').fillna(0)
 
-# Convertir los valores a numéricos
-tabla_calor = tabla_calor.apply(pd.to_numeric, errors='coerce')
 
 
 #######################
